920dae89090ce36efeac438a4d86b046e47952c4
Maarten Pronk <git@evetion.nl>
2022-06-04 21:39:11 +0200

    Ported wrong function.

--- src/compat.jl
+++ src/compat.jl
@@ -1,26 +1,5 @@
-# from https://github.com/JuliaLang/julia/pull/31834/files backported to Julia 1.6
+# from https://github.com/JuliaLang/julia/pull/38675/files avaible in julia 1.7
 
 @static if VERSION < v"1.7"
-    """
-        findall(pattern::Union{AbstractString,Regex}, string::AbstractString; overlap::Bool=false)
-
-    Return a `Vector{UnitRange{Int}}` of all the matches for `pattern` in `string`.
-    Each element of the returned vector is a range of indices where the
-    matching sequence is found, like the return value of [`findnext`](@ref).
-
-    If `overlap=true`, the matching sequences are allowed to overlap indices in the
-    original string, otherwise they must be from distinct character ranges.
-    """
-    function Base.findall(t::Union{AbstractString,Regex}, s::AbstractString; overlap::Bool=false)
-        found = UnitRange{Int}[]
-        i, e = firstindex(s), lastindex(s)
-        while true
-            r = findnext(t, s, i)
-            isnothing(r) && return found
-            push!(found, r)
-            j = overlap || isempty(r) ? first(r) : last(r)
-            j > e && return found
-            @inbounds i = nextind(s, j)
-        end
-    end
+    Base.findall(c::AbstractChar, s::AbstractString) = findall(isequal(c), s)
 end
